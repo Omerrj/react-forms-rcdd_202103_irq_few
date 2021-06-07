31b5586472f035997541bc1e402307b75ab02aae
Omer Sabah <omerrj99@gmail.com>
2021-06-07 23:31:07 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './components/Form'
 import ParentComponent from './components/ParentComponent'
+
+
 ReactDOM.render(
   <div>
     <ParentComponent />
