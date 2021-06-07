14b30f0f4246e3b998dad4f7d0a91be95a9dfae7
Omer Sabah <omerrj99@gmail.com>
2021-06-07 23:33:01 +0000

    Automatically backed up by Learn

--- src/components/DisplayData.js
+++ src/components/DisplayData.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function({formData}) {
     return (
       <div>
-        <h1>{firstName}</h1>
-        <h1>{lastName}</h1>
+        <h1>{formData.firstName}</h1>
+        <h1>{formData.lastName}</h1>
       </div>
     )
 }
